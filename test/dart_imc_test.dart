219f1ad8c3f6015d7b2581b696c3fb6a0122c3b4
stallone-dev <stallone.developer@gmail.com>
2025-03-19 23:45:49 -0300

    test(basic): Add basic test about IMC

--- test/dart_imc_test.dart
+++ test/dart_imc_test.dart
@@ -1,8 +1,11 @@
-import 'package:dart_imc/dart_imc.dart';
+import 'package:dart_imc/imc_categories.enum.dart';
+import 'package:dart_imc/person.class.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('calculate', () {
-    expect(calculate(), 42);
+    Person a = Person("Stal", 55, 1.4);
+
+    expect(a.getCategory().name, "severe_thinness");
   });
 }
